62f9b69c3ebfa9c55923d64d0670866926c622c5
Caio Henrique <142283179+CaioHPM@users.noreply.github.com>
2023-10-01 20:50:31 -0300

    Exercicio8.c

---
+++ Exercicio8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+    int vetor[6], i;
+
+    for ( i = 0; i < 6; i++)
+    {
+        printf("Digite o valor da %d° posicao do vetor:\n", i + 1);
+        scanf("%d", &vetor[i]);
+
+        getchar();
+    }
+    
+    printf("A ordem do vetor inversamente e:");
+
+    for ( i = 5; i > 0; i = i - 1)
+    {
+        printf("%d - ", vetor[i]);
+    }
+    
+
+    return 0;
+}
